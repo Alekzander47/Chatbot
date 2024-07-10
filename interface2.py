4c5788d9ecddf92a11d669045ea31870ce372706
Ajay singh <aalexander4700@gmail.com>
2024-07-10 11:17:08 +0530

    RAG Initalized

---
+++ interface2.py
@@ -0,0 +1,20 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from langchain_community.llms import Ollama
+
+# My local documents
+documents = SimpleDirectoryReader("data").load_data()
+
+# Embeddings model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# Language model
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+# Create index
+index = VectorStoreIndex.from_documents(documents)
+
+# Perform RAG query
+query_engine = index.as_query_engine()
+response = query_engine.query("What are the 5 stages of RAG?")
+print(response)
